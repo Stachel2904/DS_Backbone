c49541363530bfc625e5571d4a12a64916f71b51
Stachel2904 <devin97@gmx.de>
2025-03-09 15:18:19 +0100

    ADDED comments to logging

--- ModuleCore/Runtime/Logging/Log.cs
+++ ModuleCore/Runtime/Logging/Log.cs
@@ -6,36 +6,68 @@ using UnityEngine;
 
 namespace DivineSkies.Modules.Logging
 {
-    public enum MessageType { ScreenMessage, LogMessage, Log, Warning, Error }
+    internal enum MessageType { ScreenMessage, LogMessage, Log, Warning, Error }
 
+    /// <summary>
+    /// Use this class to print messages to Logs
+    /// </summary>
     public class Log : ModuleBase<Log>
     {
-        public virtual void PrintLogMessage(string sender, string message, MessageType type)
+        /// <summary>
+        /// Use this to display ScreenMessages ingame
+        /// </summary>
+        public event Action<string> OnScreenMessagePrinted;
+
+        /// <summary>
+        /// Use this to display LogMessages ingame
+        /// </summary>
+        public event Action<string> OnLogMessagePrinted;
+
+        /// <summary>
+        /// Prints a message to unity console or text file if not in editor.
+        /// </summary>
+        internal void PrintLogMessage(string sender, string message, MessageType type)
         {
 #if UNITY_EDITOR
             sender = $"<b><color=orange>[{sender}]</color></b> ";
             if (type is MessageType.ScreenMessage or MessageType.LogMessage or MessageType.Log)
+            {
                 Debug.Log(sender + message);
-            if (type == MessageType.Warning)
-                Debug.LogWarning(sender + message);
-            if (type == MessageType.Error)
-                Debug.LogError(sender + message);
-#endif
+            }
 
-#if !UNITY_EDITOR
+            if (type == MessageType.Warning)
+            {
+                Debug.LogWarning(sender + message);
+            }
+
+            if (type == MessageType.Error)
+            {
+                Debug.LogError(sender + message);
+            }
+#else
             sender = $"[{sender}] ";
             AddLogLine($"[{DateTime.Now:HH:mm:ss}] {type.ToString().ToUpper()}: " + sender + message);
 #endif
+            if(type is MessageType.ScreenMessage)
+            {
+                OnScreenMessagePrinted?.Invoke(message);
+            }
+            else if (type is MessageType.ScreenMessage)
+            {
+                OnLogMessagePrinted?.Invoke(message);
+            }
         }
 
 #if !UNITY_EDITOR
         private void AddLogLine(string message)
         {
-            DateTime startTime = DateTime.Now.AddSeconds(UnityEngine.Time.realtimeSinceStartup * -1);
+            DateTime startTime = DateTime.Now.AddSeconds(Time.realtimeSinceStartup * -1);
             string path = $@"{Application.persistentDataPath}/Logs/{startTime.ToString().Replace(".", "").Replace(":", "").Replace(" ", "_")}.txt";
 
             if (!Directory.Exists($@"{Application.persistentDataPath}/Logs"))
+            {
                 Directory.CreateDirectory($@"{Application.persistentDataPath}/Logs");
+            }
 
             StreamWriter writer = File.Exists(path) ? new StreamWriter(path, true) : new StreamWriter(File.Create(path));
             writer.WriteLine(message);
--- ModuleCore/Runtime/Logging/PrintExtesions.cs
+++ ModuleCore/Runtime/Logging/PrintExtesions.cs
@@ -2,16 +2,45 @@
 
 public static class PrintExtesions
 {
+    /// <summary>
+    /// Use this to log ingame Messages
+    /// </summary>
+    /// <param name="onScreen">Is this an on screen message?</param>
     public static void PrintMessage(this object obj, string message, bool onScreen = true) => Print(obj, message, onScreen ? MessageType.ScreenMessage : MessageType.LogMessage);
+
+    /// <summary>
+    /// Use this to print a normal Log to console (in editor) or file (in build)
+    /// </summary>
     public static void PrintLog(this object obj, object msgObj) => Print(obj, msgObj.ToString(), MessageType.Log);
+
+    /// <summary>
+    /// Use this to print a normal Log to console (in editor) or file (in build)
+    /// </summary>
     public static void PrintLog(this object obj, string message) => Print(obj, message, MessageType.Log);
+
+    /// <summary>
+    /// Use this to print a Warning to console (in editor) or file (in build)
+    /// </summary>
     public static void PrintWarning(this object obj, object msgObj) => Print(obj, msgObj.ToString(), MessageType.Warning);
+
+    /// <summary>
+    /// Use this to print a Warning to console (in editor) or file (in build)
+    /// </summary>
     public static void PrintWarning(this object obj, string message) => Print(obj, message, MessageType.Warning);
+
+    /// <summary>
+    /// Use this to print a Error to console (in editor) or file (in build)
+    /// </summary>
     public static void PrintError(this object obj, object msgObj) => Print(obj, msgObj.ToString(), MessageType.Error);
+
+    /// <summary>
+    /// Use this to print a Error to console (in editor) or file (in build)
+    /// </summary>
     public static void PrintError(this object obj, string message) => Print(obj, message, MessageType.Error);
+
     private static void Print(object obj, string message, MessageType type = MessageType.Log)
     {
-        var className = obj.GetType().ToString().Remove(0, obj.GetType().ToString().LastIndexOf('.') + 1);
+        string className = obj.GetType().ToString().Remove(0, obj.GetType().ToString().LastIndexOf('.') + 1);
         Log.Main.PrintLogMessage(className, message, type);
     }
 }
