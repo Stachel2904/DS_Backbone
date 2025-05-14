5415fd55d73f5ee820fcf0013f3893e2c65690e8
Stachel2904 <devin97@gmx.de>
2025-05-14 16:52:19 +0200

    CHANGED generic selection popup to actually be generic (v1.0.3)

--- _Modules/PopupModule/Runtime/Popups/GenericSelectionPopup.cs
+++ _Modules/PopupModule/Runtime/Popups/GenericSelectionPopup.cs
@@ -7,16 +7,29 @@ namespace DivineSkies.Modules.Popups
     /// <summary>
     /// Use this Popup to display a set of Items
     /// </summary>
-    public class GenericSelectionPopup : NotificationPopup
+    public abstract class GenericSelectionPopup<TItem, TData> : NotificationPopup where TItem : UiItemBase
     {
         [SerializeField] private Transform _selectionParent;
 
-        public void Init<TItem, TData>(string title, string content, TData[] datas, Action<TItem, TData> onSetup, Action onClose = null, bool openAfterwards = true) where TItem : UiItemBase
+        private PooledListDisplay<TItem, TData> _selectionList;
+
+        protected override void OnCreation()
+        {
+            base.OnCreation();
+            for (int i = 0; i < _selectionParent.childCount; i++)
+            {
+                Destroy(_selectionParent.GetChild(0).gameObject);
+            }
+
+            _selectionList = new PooledListDisplay<TItem, TData>(_selectionParent, SetupSelectionItem);
+        }
+        public void Init(string title, string content, TData[] datas, Action onClose = null, bool openAfterwards = true)
         {
             Init(title, content, openAfterwards, onClose);
 
-            PooledListDisplay<TItem, TData> list = new PooledListDisplay<TItem, TData>(_selectionParent, onSetup);
-            list.SetData(datas);
+            _selectionList.SetData(datas);
         }
+
+        protected abstract void SetupSelectionItem(TItem item, TData data);
     }
 }
