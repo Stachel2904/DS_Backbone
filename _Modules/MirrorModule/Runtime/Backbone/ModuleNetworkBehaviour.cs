63a8cd56cb771f236c29123469cacb2a00b66c19
Stachel2904 <devin97@gmx.de>
2025-09-28 19:14:14 +0200

    ADDED mirror module (0.0.1)

---
+++ _Modules/MirrorModule/Runtime/Backbone/ModuleNetworkBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using DivineSkies.Modules;
+using DivineSkies.Modules.Core;
+using Mirror;
+
+namespace DivineSkies.MirrorModule
+{
+    public class ModuleNetworkBehaviour<TNetworkModule> : NetworkBehaviour where TNetworkModule : ModuleBase
+    {
+        public TNetworkModule ParentModule { get; private set; }
+
+        public NetworkIdentity Identity { get; private set; }
+
+        internal void Initialize()
+        {
+            ParentModule = ModuleController.Get<TNetworkModule>();
+
+            if (ParentModule.gameObject.TryGetComponent(out NetworkIdentity comp))
+            {
+                Identity = comp;
+            }
+            else
+            {
+                Identity = ParentModule.gameObject.AddComponent<NetworkIdentity>();
+            }
+        }
+    }
+}
---
+++ _Modules/MirrorModule/Runtime/Backbone/NetworkModuleBase.cs
@@ -0,0 +1,26 @@
+﻿using DivineSkies.Modules;
+using DivineSkies.Modules.Core;
+
+namespace DivineSkies.MirrorModule
+{
+    public abstract class NetworkModuleBase<TNetworkBehaviour, TModule> : ModuleBase<TModule> where TModule : ModuleBase where TNetworkBehaviour : ModuleNetworkBehaviour<TModule>
+    {
+        public TNetworkBehaviour NetworkBehaviour { get; private set; }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            if (gameObject.TryGetComponent(out TNetworkBehaviour comp))
+            {
+                NetworkBehaviour = comp;
+            }
+            else
+            {
+                NetworkBehaviour = gameObject.AddComponent<TNetworkBehaviour>();
+            } 
+
+            NetworkBehaviour.Initialize();
+        }
+    }
+}
